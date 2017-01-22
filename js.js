00729b5a99d6711c1d9b43be11027d23e64f2d56
viacheslav-boiarchuk <falanor@mail.ru>
2017-01-22 19:58:25 +0200

    test2 (cherry picked from commit 1627dab)

--- js.js
+++ js.js
@@ -2,3 +2,7 @@
  * Created by user on 22.06.2016.
  */
 console.log('HI HI 1111!');
+console.log('HI HI 1111!');
+console.log('HI HI 1111!');
+console.log('HI HI 1111!');
+console.log('HI HI 1111!');
